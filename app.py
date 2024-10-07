fd3c171b8303d4b1c347f3686b2ed0795b3e041d
Ronaldo Rivero Gonzales <Ronaldorivero3@Gmail.com>
2024-10-06 22:49:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 
-from sklearn.decomposition import PCA
-from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
+
+from pca import mostrar_pca
+from kmean import mostrar_cluster_kmeans
+from preparacion_datos import mostrar_cargar_datos
+from preparacion_datos import mostrar_analisis_datos
 
 st.set_page_config(page_title='Trabajo Práctico'
                    , page_icon=':shark:'
@@ -19,45 +20,11 @@ opciones = ["Cargar Datos",
             'Analisis de Datos',
             'Limpiar y Preparar Datos',
             'PCA',
-            'Cluster']
+            'KMeans']
 opcion = st.sidebar.radio('Selecciona una Opcion', opciones)
 
-@st.cache_data
-def cargar_datos(archivo):
-    if archivo.name.endswith('csv'):
-        df = pd.read_csv(archivo)
-    elif archivo.name.endswith('xls'):
-        df = pd.read_excel(archivo)
-    else:
-        raise Exception('Este formato no es compatible')
-    return df
 
-
-if opcion == 'Cargar Datos':
-    st.subheader('Cargar Datos')
-    archivo = st.file_uploader('Cargar Archivo CSV o XLS', type=['csv', 'xls'])
-    if archivo:
-        df = cargar_datos(archivo)
-        if df is not None:
-            st.session_state.df = df
-            st.warning('El archivo se ha cargado correctamente')
-            st.write('El dataset tiene', df.shape[0], 'filas y', df.shape[1], 'columnas')
-            st.write(df)
-    else:
-        st.warning('Por favor carga un archivo')
-
-elif opcion == 'Analisis de Datos':
-    st.subheader('Analisis de Datos')
-    if 'df' not in st.session_state:
-        st.warning('Por favor carga un archivo')
-    else:
-        df = st.session_state.df
-        st.subheader('Primeras 5 filas')
-        st.write(df.head())
-        st.subheader('Información descriptiva del dataset')
-        st.write(df.describe())
-
-elif opcion == 'Limpiar y Preparar Datos':
+def mostrar_limpiar_preparar_datos():
     st.subheader('Limpiar y Preparar Datos')
     if 'df' not in st.session_state:
         st.warning('Por favor carga un archivo')
@@ -128,7 +95,7 @@ elif opcion == 'Limpiar y Preparar Datos':
                 with st.spinner('Normalizando valores'):
                     st.write('Valores normalizados')
                     scaler = MinMaxScaler()
-                    df_normalizado =  pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
+                    df_normalizado = pd.DataFrame(scaler.fit_transform(df), columns=df.columns)
 
                 st.session_state.df_normalizado = df_normalizado
                 st.success('Valores normalizados')
@@ -151,75 +118,17 @@ elif opcion == 'Limpiar y Preparar Datos':
             else:
                 st.write("No ha seleccionado columnas.")
 
+if opcion == 'Cargar Datos':
+    mostrar_cargar_datos()
+
+elif opcion == 'Analisis de Datos':
+    mostrar_analisis_datos()
+
+elif opcion == 'Limpiar y Preparar Datos':
+    mostrar_limpiar_preparar_datos()
+
 elif opcion == 'PCA':
-    st.subheader('PCA')
-    if 'df_normalizado' not in st.session_state:
-        st.warning('Por favor carga un archivo y normalizalo')
-    else:
-        df_normalizado = st.session_state.df_normalizado
+    mostrar_pca()
 
-        pca = PCA()
-        datos_pca = pca.fit_transform(df_normalizado)
-
-        col1, col2, col3 = st.columns(3)
-        with col1:
-            st.write('Varianza Explicada:', pca.explained_variance_)
-        with col2:
-            st.write('Varianza Explicada:', pca.explained_variance_ratio_)
-        with col3:
-            varianza_acumulada = np.cumsum(pca.explained_variance_ratio_)
-            st.write('Varianza Acumulada:', varianza_acumulada)
-
-        fig = plt.figure(figsize=(8, 8))
-        plt.plot(varianza_acumulada)
-        plt.hlines(y=0.9, xmin=0, xmax=4, colors='r', linestyles='--')
-        plt.xlabel('Numero de Componentes Principales')
-        plt.ylabel('Varianza Acumulada')
-        st.pyplot(fig)
-
-elif opcion == 'Cluster':
-    st.subheader("Clustering - KMeans")
-    if 'df_normalizado' not in st.session_state:
-        st.warning('Por favor carga un archivo y normalizalo')
-    else:
-        df_normalizado = st.session_state.df_normalizado
-
-        n_clusters = st.slider('Número de Clusters', 2, 10, 3)
-        random_state = st.slider('Random State', 0, 100, 42)
-        max_iter = st.slider('Numero de Iteraciones', 100, 1000, 300)
-
-        kmeans = KMeans(n_clusters=n_clusters, random_state=random_state, max_iter=max_iter).fit(
-            df_normalizado.values)
-
-        silhouette = silhouette_score(df_normalizado.values, kmeans.labels_)
-
-        col1, col2 = st.columns(2)
-        columnas = df_normalizado.columns.tolist()
-        with col1:
-            fig = plt.figure(figsize=(6, 6))
-            plt.scatter(df_normalizado.iloc[:, 0], df_normalizado.iloc[:, 1], c=kmeans.labels_, s=150,
-                        marker='8')
-            plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], c='red', s=350, marker='+')
-            plt.xlabel(columnas[0])
-            plt.ylabel(columnas[1])
-            plt.title('KMeans Clustering')
-            st.pyplot(fig)
-        with col2:
-            st.markdown('## Medidas de Evaluación')
-            st.write('Numero de Clusters:', kmeans.n_clusters)
-            st.write('Inercia:', kmeans.inertia_)
-            st.write('Silhouette Score:', silhouette)
-
-        st.subheader('Método del Codo')
-        inertias = []
-        for i in range(1, 11):
-            kmeans = KMeans(n_clusters=i, random_state=random_state).fit(df_normalizado.values)
-            inertias.append(kmeans.inertia_)
-
-        fig = plt.figure(figsize=(6, 6))
-        plt.scatter(range(1, 11), inertias, c='red', s=150, marker='o', edgecolor='black')
-        plt.plot(range(1, 11), inertias)
-        plt.xlabel('Número de Clusters')
-        plt.ylabel('Inertia')
-        plt.title('Método del Codo')
-        st.pyplot(fig)
+elif opcion == 'KMeans':
+    mostrar_cluster_kmeans()
